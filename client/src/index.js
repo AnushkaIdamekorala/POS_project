7b40daac330ba43e7d2702616efc0c6e7330a51e
Anushka <anushka0731@gmail.com>
2019-03-08 14:38:13 +0530

    14.38 Auth remove

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import Auth from "./Auth";
+
 import Layer from "./Layer";
 
 import * as serviceWorker from "./serviceWorker";
